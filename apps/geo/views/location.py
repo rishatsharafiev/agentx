1556e52c69e1d64bd926abc10095a17649914e68
Rishat Sharafiev <rishatsharafiev@ya.ru>
2019-04-15 02:36:04 +0300

    Add datetime filter

--- apps/geo/views/location.py
+++ apps/geo/views/location.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.utils import timezone
 from utils.decorators import json_request, json_response
 from utils.views import BaseView
@@ -12,21 +13,37 @@ class LocationView(BaseView):
     @json_response
     def get(self, request, pk=None):
         """Get location"""
-        locations = Location.objects.filter(employee__pk=pk).order_by('-created_at')
+        try:
+            created_at__gte = request.GET.get('created_at__gte', '1900-01-01')
+            created_at__lte = request.GET.get('created_at__lte', '3000-01-01')
 
-        response = {
-            "apiVersion": "1.0",
-            "id": pk,
-            "method": "GET",
-            "data": {
-                "kind": "employee",
-                "fields": "latitude,longitude,created_at",
-                "items": [{"latitude": location.latitude,
-                           "longitude": location.longitude,
-                           "created_at": timezone.localtime(location.created_at),
-                           } for location in locations]
+            locations = Location.objects.filter(created_at__gte=created_at__gte,
+                                                created_at__lte=created_at__lte,
+                                                employee__pk=pk).order_by('-created_at')
+            items = [{
+                "latitude": location.latitude,
+                "longitude": location.longitude,
+                "created_at": timezone.localtime(location.created_at)
+            } for location in locations]
+            
+            response = {
+                "apiVersion": "1.0",
+                "id": pk,
+                "method": "GET",
+                "data": {
+                    "kind": "employee",
+                    "fields": "latitude,longitude,created_at",
+                    "items": items
+                }
             }
-        }
+        except ValidationError as exp:
+            response = {
+                'error': {
+                    'code': 422,
+                    'message': 'Validation Error',
+                    'errors': str(exp)
+                }
+            }
 
         return response
 
