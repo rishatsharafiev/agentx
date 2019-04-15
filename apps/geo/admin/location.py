bb9a063b14a69ad099896ddc8c0b9b6f103fb4a2
Rishat Sharafiev <rishatsharafiev@ya.ru>
2019-04-15 11:12:41 +0300

    Add last locations

--- apps/geo/admin/location.py
+++ apps/geo/admin/location.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 class LocationAdmin(admin.ModelAdmin):
     """Location Admin"""
 
-    list_display = ('id', 'employee', 'latitude', 'latitude', 'created_at',)
+    list_display = ('id', 'employee', 'latitude', 'longitude', 'created_at',)
     search_fields = ('employee__first_name', 'employee__last_name')
     list_per_page = 30
     readonly_fields = ('created_at',)
---
+++ apps/geo/views/employee_location.py
@@ -0,0 +1,35 @@
+from django.utils import timezone
+from utils.decorators import json_response
+from utils.views import BaseView
+
+from ..models import Location
+
+
+class EmployeeLocationView(BaseView):
+    """Employee Location View"""
+
+    @json_response
+    def get(self, request):
+        """Get location"""
+        locations = Location.objects.prefetch_related('employee') \
+            .order_by('employee_id', '-created_at').distinct('employee_id')
+
+        items = [{
+            "first_name": location.employee.first_name,
+            "last_name": location.employee.last_name,
+            "latitude": location.latitude,
+            "longitude": location.longitude,
+            "created_at": timezone.localtime(location.created_at)
+        } for location in locations]
+
+        response = {
+            "apiVersion": "1.0",
+            "method": "GET",
+            "data": {
+                "kind": "employee",
+                "fields": "first_name,last_name,latitude,longitude,created_at",
+                "items": items
+            }
+        }
+
+        return response
